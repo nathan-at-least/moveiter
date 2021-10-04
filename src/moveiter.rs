ad357225f53967a874b817603546b40c838a34bc
Nate Wilcox <nathan+dev@electriccoin.co>
2021-10-04 10:12:12 -0700

    Untested first draft of `MoveIterator::into_iter` to convert to std `Iterator`.

--- src/moveiter.rs
+++ src/moveiter.rs
@@ -1,9 +1,16 @@
+mod adapter;
+
+pub use self::adapter::StdIteratorAdapter;
 use crate::EndlessIterator;
 
 pub trait MoveIterator: Sized {
     type Item;
 
     fn into_next_option(self) -> Option<(Self, Self::Item)>;
+
+    fn into_iter(self) -> StdIteratorAdapter<Self> {
+        StdIteratorAdapter::from(self)
+    }
 }
 
 impl<T> MoveIterator for T
---
+++ src/moveiter/adapter.rs
@@ -0,0 +1,35 @@
+use crate::MoveIterator;
+
+#[derive(Debug)]
+pub struct StdIteratorAdapter<MI>(Option<MI>)
+where
+    MI: MoveIterator;
+
+impl<MI> From<MI> for StdIteratorAdapter<MI>
+where
+    MI: MoveIterator,
+{
+    fn from(mi: MI) -> Self {
+        StdIteratorAdapter(Some(mi))
+    }
+}
+
+impl<MI> Iterator for StdIteratorAdapter<MI>
+where
+    MI: MoveIterator,
+{
+    type Item = <MI as MoveIterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0.take() {
+            None => None,
+            Some(mi) => match mi.into_next_option() {
+                None => None,
+                Some((newstate, item)) => {
+                    self.0 = Some(newstate);
+                    Some(item)
+                }
+            },
+        }
+    }
+}
