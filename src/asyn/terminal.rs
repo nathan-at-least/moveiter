781edbea88f80285957ffde609b519ff39feb986
Nate Wilcox <nathan+dev@electriccoin.co>
2022-06-15 11:57:54 -0700

    Rename `next` → `atmi_next`, implement call-site example for `AsyncTerminalMoveIterator::atmi_next`.

--- src/asyn/terminal.rs
+++ src/asyn/terminal.rs
@@ -3,6 +3,24 @@ use async_trait::async_trait;
 use either::Either;
 
 /// An `AsyncTerminalMoveIterator` type produces a sequence of 0 or more `Item` values asynchronously, then produces a `Terminal` value, using move semantics.
+///
+/// # Call-site Example
+///
+/// ```
+/// # tokio::runtime::Builder::new_current_thread().build().unwrap().block_on(async {
+/// use moveiter::AsyncTerminalMoveIterator;
+///
+/// let it = 0..2;
+///
+/// let (it2, a) = it.atmi_next().await.left().unwrap();
+/// assert_eq!(a, 0);
+///
+/// let (it3, b) = it2.atmi_next().await.left().unwrap();
+/// assert_eq!(b, 1);
+///
+/// let term = it3.atmi_next().await.right().unwrap();
+/// assert_eq!((), term);
+/// # });
 #[async_trait]
 pub trait AsyncTerminalMoveIterator: Sized + Send {
     type Item;
@@ -14,9 +32,9 @@ pub trait AsyncTerminalMoveIterator: Sized + Send {
     /// The simplified interface via [async_trait](::async_trait) looks like:
     ///
     /// ```ignore
-    /// async fn next(self) -> Either<(Self, Self::Item), Self::Terminal>;
+    /// async fn atmi_next(self) -> Either<(Self, Self::Item), Self::Terminal>;
     /// ```
-    async fn next(self) -> Either<(Self, Self::Item), Self::Terminal>;
+    async fn atmi_next(self) -> Either<(Self, Self::Item), Self::Terminal>;
 }
 
 /// Any [Iterator] + [Sized] + [Send] is an [AsyncTerminalMoveIterator].
@@ -28,10 +46,10 @@ where
     type Item = I::Item;
     type Terminal = ();
 
-    async fn next(mut self) -> Either<(Self, Self::Item), Self::Terminal> {
+    async fn atmi_next(mut self) -> Either<(Self, Self::Item), Self::Terminal> {
         use Either::*;
 
-        if let Some(item) = Iterator::next(&mut self) {
+        if let Some(item) = self.next() {
             Left((self, item))
         } else {
             Right(())
