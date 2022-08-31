8d18b78b7f47acbe9fb881ac8bc6dd2cc74a60ce
user <user@penguin>
2022-08-31 08:51:43 -0700

    Add "At A Glance" and "Blanket Implementations" sections.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,38 @@
 //! with the item, whereas when iteration completes, no new iterator is available. This prevents
 //! incorrect states during type checking avoiding the need for runtime safety mechanisms like
 //! [std::iter::FusedIterator].
+//!
+//! There are two axes defining the trait family: sync/async and the kind of
+//! termination. Termination can be "endless", "finite", or "terminal", where "terminal" produces
+//! a typed value as the final step. This leads to six traits.
+//!
+//! This allows implementors and consumers to select the appropriate trait with fine-grained
+//! semantics.
+//!
+//! # At a Glance
+//!
+//! Each trait is based on a single provided `into_next` method:
+//!
+//! | Trait                       | Method |
+//! |-----------------------------|---------------------------------------------------------------------------|
+//! | [EndlessMoveIterator]       | `fn into_next(self) -> (Self, Self::Item);`                               |
+//! | [FiniteMoveIterator]        | `fn into_next(self) -> Option<(Self, Self::Item)>;`                       |
+//! | [TerminalMoveIterator]      | `fn into_next(self) -> Either<(Self, Self::Item), Self::Terminal>;`       |
+//! | [AsyncEndlessMoveIterator]  | `async fn into_next(self) -> (Self, Self::Item);`                         |
+//! | [AsyncFiniteMoveIterator]   | `async fn into_next(self) -> Option<(Self, Self::Item)>;`                 |
+//! | [AsyncTerminalMoveIterator] | `async fn into_next(self) -> Either<(Self, Self::Item), Self::Terminal>;` |
+//!
+//! # Blanket Implementations
+//!
+//! There are blanket impls of any [std::iter::Iterator] for any of the non-endless traits:
+//!
+//! - [FiniteMoveIterator]
+//! - [TerminalMoveIterator]
+//! - [AsyncFiniteMoveIterator]
+//! - [AsyncTerminalMoveIterator]
+//!
+//! This allows any interfaces that accept these generic bounds to take a [std::iter::Iterator]
+//! value seamlessly.
 mod asyn;
 mod syn;
 
