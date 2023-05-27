5ad9d7eb6c9366da71b6a511ab38e11e9266fd8f
Nate Wilcox <nathan+dev@electriccoin.co>
2023-05-27 10:31:24 -0700

    Fix a typo.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! A family of iteration traits which use move semantics.
 //!
-//! Move semantics consume and iterator, and if there is a next item, a new iterator is returned
+//! Move semantics consume an iterator, and if there is a next item, a new iterator is returned
 //! with the item, whereas when iteration completes, no new iterator is available. This prevents
 //! incorrect states during type checking avoiding the need for runtime safety mechanisms like
 //! [std::iter::FusedIterator].
